263babdcf1ed7eb45f8cdc1de0ab984fd060adad
O.R. Rahman <108026655+kabirinfoga@users.noreply.github.com>
2022-09-01 21:31:54 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:gimpact/Authentication/signin.dart';
+import 'package:gimpact/Home/home.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.purple
+      ),
+      debugShowCheckedModeBanner: false,
+      home:const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text("Something went wrong!"),
+              );
+            } else if (snapshot.hasData) {
+              return const Home();
+            } else {
+              return const SignIn();
+            }
+          }),
+    );
+  }
+}
